fc69fcb9f27e3139a5402e7f229f1cc69d664e28
karcikovas <viktoras.karcikovas@gmail.com>
2020-02-24 18:09:41 +0200

    Projects are fetched if search value is less that 3

--- src/js/Pages/Projects/Partials/Search/Search.js
+++ src/js/Pages/Projects/Partials/Search/Search.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { searchProject } from '../../../../../core/Project/ProjectsActions'
+import { searchProject, getProjects} from '../../../../../core/Project/ProjectsActions'
 import './Search.scss'
 
-const Search = ({ searchForProject }) => {
+const Search = ({ searchForProject, returnDefaultList, amount, page }) => {
     const handleChange = (event) => {
         const value = event.target.value;
 
         if (value.length > 3) searchForProject(value);
+
+        if (value.length <= 3)returnDefaultList(amount, page);
     }
 
     return(
@@ -23,10 +25,19 @@ const Search = ({ searchForProject }) => {
 
 Search.propTypes = {
     searchForProject: PropTypes.func.isRequired,
+    returnDefaultList: PropTypes.func.isRequired,
+    amount: 10,
+    page: 1,
 }
+
+const mapStateToProps = state => ({
+    amount: state.settings.per_page,
+    page: state.settings.page,
+})
 
 const mapDispatchToProps = dispatch => ({
     searchForProject: (name) => dispatch(searchProject(name)),
+    returnDefaultList: (amount, page) => dispatch(getProjects(amount, page))
 })
 
-export default connect(null ,mapDispatchToProps)(Search);
+export default connect(mapStateToProps ,mapDispatchToProps)(Search);
